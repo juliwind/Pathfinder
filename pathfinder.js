4fd228f5c7db106d509435bf545dd94c0235482e
juliwind <julianwindheuser@gmail.com>
2021-07-21 23:53:41 +0200

    startpoint location done

--- pathfinder.js
+++ pathfinder.js
@@ -1,7 +1,10 @@
-let cols = prompt("Wie viele Spalten? ", );
-let rows = prompt("Wie viele Reihen? ", );
+let cols = prompt("Number of columns: ", );
+let rows = prompt("Number of rows: ", );
 let canvas_pg = document.getElementById("canvasPlayground");
 let ctx_pg = canvas_pg.getContext("2d");
+let loopEnd_start = false;
+let startX = null;
+let startY = null;
 
 let array_field = new Array();
 for (i = 0; i < cols; i++) {
@@ -21,7 +24,7 @@ function grid() {
         ctx_pg.lineTo(i, 800);
         ctx_pg.stroke();
     }
-}
+
 // HORIZONTAL
     for(i = 800/rows; i < 800; i+= (800 / rows)) {
         ctx_pg.beginPath();
@@ -30,10 +33,32 @@ function grid() {
         ctx_pg.lineTo(1200, i);
         ctx_pg.stroke();
     }
+}
 
+function setStartpoint() {
+    if (!loopEnd_start) {
+        document.getElementById('start').classList.add("buttonActiv");
+        canvas_pg.addEventListener("click", onClickStartpoint);
+        setTimeout(setStartpoint);
+    }
+}
 
+function onClickStartpoint(e) {
+    for(i = 0; i < 1200; i+= (1200 / cols)) {
+        if (e.clientX > i + 10 && e.clientX < i + (1200 / cols) + 10) {
+            //console.log("cols: ", i / (1200 / cols))
+        }
+    }
 
+    for (j = 0; j < 800; j+= (800 / rows)) {
+        if (e.clientY > j + 185 && e.clientY < j + (800 / rows) + 185) {
+            console.log("rows: ", j / (800 / rows))
+        }
+    }
+}
 grid()
 
-
+/*
+-10
+*/
 
