f0f81cd04286418ba106508fa98aedfbc232778a
juliwind <julianwindheuser@gmail.com>
2021-07-23 00:09:57 +0200

    delete Barriers
    added delete Barrier button, some bugs

--- pathfinder.js
+++ pathfinder.js
@@ -7,13 +7,17 @@ let startX = null;
 let startY = null;
 let endX = null;
 let endY = null;
-let barrierX = null;
-let barrierY = null;
+let place_barrierX = null;
+let place_barrierY = null;
+let delete_barrierX = null;
+let delete_barrierY = null;
 let array_field = new Array();
 let cols = 15;
 let rows = 10;
-let barrier_counter = 0;
-let barrier_loop
+let place_barrier_counter = 0;
+let place_barrier_loop = null;
+let delete_barrier_counter = 0;
+let delete_barrier_loop = null;
 
 setup()
 function setup() {
@@ -158,38 +162,38 @@ function onClickEndpoint(e) {
     document.getElementById('end').classList.remove("buttonActiv");
 }
 
-//BARRIERS
+//PLACE BARRIERS
 function placeBarriers() {
     if (!loopEnd_barriers) {
         document.getElementById('placeBarriers').classList.add("buttonActiv");
         canvas_pg.addEventListener("mousedown", onClickPlaceBarriers);
-        barrier_loop = setTimeout(placeBarriers);
+        place_barrier_loop = setTimeout(placeBarriers);
     }
 }
 
 function onClickPlaceBarriers(e) {
     for(i = 0; i < 1200; i+= (1200 / cols)) {
         if (e.clientX > i + 10 && e.clientX < i + (1200 / cols) + 10) {
-            barrierX = i / (1200 / cols) + 1
+            place_barrierX = i / (1200 / cols) + 1
         }
     }
     for (j = 0; j < 800; j+= (800 / rows)) {
         if (e.clientY > j + 140 && e.clientY < j + (800 / rows) + 140) {
-            barrierY = j / (800 / rows) + 1
+            place_barrierY = j / (800 / rows) + 1
         }
     }
 
     ctx_pg.beginPath();
     ctx_pg.fillStyle = "black";
-    ctx_pg.fillRect(barrierX * (1200 / cols) - (1200 / cols) + 1, barrierY * (800 / rows) - (800 / rows) + 1, 1200 / cols - 2, 800 / rows - 2);
+    ctx_pg.fillRect(place_barrierX * (1200 / cols) - (1200 / cols) + 1, place_barrierY * (800 / rows) - (800 / rows) + 1, 1200 / cols - 2, 800 / rows - 2);
     ctx_pg.stroke();
 }
 
 function placeBarriersCounter() {
-    barrier_counter++
-    if (barrier_counter % 2 == 0) {
+    place_barrier_counter++
+    if (place_barrier_counter % 2 == 0) {
         placeBarriersEnd();
-        clearTimeout(barrier_loop);
+        clearTimeout(place_barrier_loop);
 
     }
     else {
@@ -200,6 +204,51 @@ function placeBarriersCounter() {
 function placeBarriersEnd() {
     canvas_pg.removeEventListener("mousedown", onClickPlaceBarriers);
     document.getElementById('placeBarriers').classList.remove("buttonActiv");
+
+}
+
+//DELETE BARRIERS
+function deleteBarriers() {
+    if (!loopEnd_delete_barriers) {
+        document.getElementById('deleteBarriers').classList.add("buttonActiv");
+        canvas_pg.addEventListener("mousedown", onClickDeleteBarriers);
+        barrier_loop = setTimeout(deleteBarriers);
+    }
+}
+
+function onClickDeleteBarriers(e) {
+    for(i = 0; i < 1200; i+= (1200 / cols)) {
+        if (e.clientX > i + 10 && e.clientX < i + (1200 / cols) + 10) {
+            delete_barrierX = i / (1200 / cols) + 1
+        }
+    }
+    for (j = 0; j < 800; j+= (800 / rows)) {
+        if (e.clientY > j + 140 && e.clientY < j + (800 / rows) + 140) {
+            delete_barrierY = j / (800 / rows) + 1
+        }
+    }
+
+    ctx_pg.beginPath();
+    ctx_pg.fillStyle = "black";
+    ctx_pg.fillRect(delete_barrierX * (1200 / cols) - (1200 / cols) + 1, delete_barrierY * (800 / rows) - (800 / rows) + 1, 1200 / cols - 2, 800 / rows - 2);
+    ctx_pg.stroke();
+}
+
+function deleteBarriersCounter() {
+    delete_barrier_counter++
+    if (delete_barrier_counter % 2 == 0) {
+        deleteBarriersEnd();
+        clearTimeout(delete_barrier_loop);
+
+    }
+    else {
+        placeBarriers();
+
+    }
+}
+function deleteBarriersEnd() {
+    canvas_pg.removeEventListener("mousedown", onClickDeleteBarriers);
+    document.getElementById('deleteBarriers').classList.remove("buttonActiv");
 
 }
 
